08eb093f0fbc09242b38e11caea4659d627fe2d9
MichalMakosiewicz <36437590+MichalMakosiewicz@users.noreply.github.com>
2018-03-26 21:18:31 +0200

    Exercise Tic, Tak, Toe
    Add outline of "choose by player" iin tic tak toe game

---
+++ Exercise 27.py
@@ -0,0 +1,14 @@
+game = [[8,8,8],
+        [8,8,8],
+        [8,8,8]]
+
+
+
+
+fun = True
+while fun == True:
+
+    a = input("Który rząd wybierasz? (1/2/3): ")
+    b = input("Którą komórkę wybierasz? (1/2/3): ")
+
+    print("")
