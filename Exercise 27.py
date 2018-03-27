44dcc506f2dd696e9c65105e0e0bc21ede3a945b
MichalMakosiewicz <36437590+MichalMakosiewicz@users.noreply.github.com>
2018-03-27 21:04:15 +0200

    Exercise Tic, tak, toe
    Design visual action to game

--- Exercise 27.py
+++ Exercise 27.py
@@ -8,7 +8,23 @@ game = [[8,8,8],
 fun = True
 while fun == True:
 
-    a = input("Który rząd wybierasz? (1/2/3): ")
-    b = input("Którą komórkę wybierasz? (1/2/3): ")
 
-    print("")
+    print("gracz nr 1:")
+
+    a = int(input("Który rząd wybierasz? (1/2/3): ")) - 1
+    b = int(input("Którą komórkę wybierasz? (1/2/3): ")) - 1
+
+    game[a][b] = 1
+
+    for i in game:
+        print(i)
+
+    print("gracz nr 2:")
+
+    c = int(input("Który rząd wybierasz? (1/2/3): ")) - 1
+    d = int(input("Którą komórkę wybierasz? (1/2/3): ")) - 1
+
+    game[c][d] = 2
+
+    for i in game:
+        print(i)
